1aeed4c10f88a29aa094edb3a995d5a34a6f8e30
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-04-01 09:54:22 +0100

    remove the 'buy' listener from the event emitter

--- event-emitters/ticketPurchase/index.js
+++ event-emitters/ticketPurchase/index.js
@@ -24,8 +24,26 @@ ticketManager.on("error", (error) => {
 });
 
 ticketManager.welcome();
+/*ticketManager.buyTicket("test@email.com", 10);
 ticketManager.buyTicket("test@email.com", 10);
 ticketManager.buyTicket("test@email.com", 10);
 ticketManager.buyTicket("test@email.com", 10);
-ticketManager.buyTicket("test@email.com", 10);
+*/
+console.log(`We have ${ticketManager.listenerCount("buy")} listener(s) for the buy event`);
+console.log(`We have ${ticketManager.listenerCount("error")} listener(s) for the error event`);
 
+const onBuy = () => {
+	console.log("I will be removed soon");
+};
+
+console.log(`We added a new event listener bringing our total
+count for the buy event to: ${ticketManager.listenerCount("buy")}`);
+
+ticketManager.on("buy", onBuy);
+ticketManager.removeAllListeners("buy");
+
+console.log(`We have ${ticketManager.listenerCount("buy")} listeners for the buy event`);
+
+ticketManager.buyTicket("test@email", 20);
+
+console.log("The last ticket was bought");
