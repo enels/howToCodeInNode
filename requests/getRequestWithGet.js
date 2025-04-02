e89a9b3391147159ebf2e6398101cf5087d10df7
Enoma Uwaifo <enomauwaifo@gmail.com>
2025-04-02 08:35:16 +0100

    fix the url

--- requests/getRequestWithGet.js
+++ requests/getRequestWithGet.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
 
-let request = https:.get('https://jsonplaceholder.typicode.com/users?_limit=2' (res) => {
+let request = https.get('https://jsonplaceholder.typicode.com/users?_limit=2', (res) => {
 	
 	if (res.statusCode !== 200) {
 		console.log(`Did not get an OK from the server. Code: ${res.statusCode}`);
@@ -10,4 +10,21 @@ let request = https:.get('https://jsonplaceholder.typicode.com/users?_limit=2' (
 
 		return;
 	}
+
+	let data = '';
+
+	// listen to when data comes from response
+	res.on('data', (chunk) => {
+		data += chunk;
+	});
+
+	res.on('close', () => {
+		console.log('Retrieved all data');
+		console.log(JSON.parse(data));
+	});
+});
+
+request.on('error', (err) => {
+
+	console.error(`Encountered an error trying to make a request: ${err.message}`);
 });
